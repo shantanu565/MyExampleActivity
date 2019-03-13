9f52f41275c57718eb5719a47f0cb0d7f7c666ba
shantanu565 <shantanu.saxena@tothenew.com>
2019-03-13 09:36:03 +0530

    inputtype update

--- app/src/main/java/com/example/myexampleactivity/MainActivity.java
+++ app/src/main/java/com/example/myexampleactivity/MainActivity.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -87,21 +88,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-
-        public void onRequestPermissionsRequest(int requestCode,String[] permissions,int[] grantresult){
-        super.onRequestPermissionsResult(requestCode,permissions,grantresult);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode==CAMERA_CODE){
-            if (grantresult[0]==PackageManager.PERMISSION_GRANTED){
+            if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
                 Toast.makeText(this,"camera permission granted",Toast.LENGTH_LONG).show();
                 Intent i2=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(i2,CAMERA_REQUEST);
             }else {
                 Toast.makeText(this,"Camera permission denied",Toast.LENGTH_LONG).show();
             }
-        }
-         }
+    }}
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -128,6 +126,15 @@ public class MainActivity extends AppCompatActivity {
         String s2=e2.getText().toString();
         String s3=e3.getText().toString();
 
+        if (s1.isEmpty()||s1.equals("")){
+            Toast.makeText(this,"Please provide your username",Toast.LENGTH_LONG).show();
+        }
+        if (s2.isEmpty()||s2.equals("")){
+            Toast.makeText(this,"Please provide your email",Toast.LENGTH_LONG).show();
+        }
+        if (s3.isEmpty()||s3.equals("")){
+            Toast.makeText(this,"Please provide your phone",Toast.LENGTH_LONG).show();
+        }
 
         Drawable drawable=imageView1.getDrawable();
         Bitmap bitmap= ((BitmapDrawable)drawable).getBitmap();
